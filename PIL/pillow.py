644e5611072e90b9445931e6e9abdcdae879ddc3
Ezz2 <94149479+Ezz2@users.noreply.github.com>
2022-06-28 14:59:44 +0200

    PIL Module

---
+++ PIL/pillow.py
@@ -0,0 +1,24 @@
+# By Ezz
+
+# -------------------------------------------------
+# -- Practical => Image Manipulation With Pillow --
+# -------------------------------------------------
+
+from PIL import Image
+
+# Open The Image
+myImage = Image.open("D:\ezz.programing\python\Images\Ezz_Image.png")
+
+# Show The Image
+myImage.show()
+
+# My Cropped Image
+myBox = (0, 0, 400, 400)
+mynewImage = myImage.crop(myBox)
+
+# Show The New Image
+mynewImage.show()
+
+# My Converted Mode Image
+myConverted = myImage.convert("L")
+myConverted.show()
