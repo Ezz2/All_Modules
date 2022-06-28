aff605c949f8014d8c60760809943b4526629887
Ezz2 <94149479+Ezz2@users.noreply.github.com>
2022-06-28 15:25:21 +0200

    Draw Love With Turtle

---
+++ Turtle/love.py
@@ -0,0 +1,16 @@
+# By Ezz
+
+from turtle import *
+
+speed(4)
+color('red')
+begin_fill()
+pensize(3)
+left(50)
+forward(133)
+circle(50, 200)
+right(500)
+circle(50, 200)
+forward(133)
+end_fill()
+done()
