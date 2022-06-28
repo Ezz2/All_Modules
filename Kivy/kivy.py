45d6ccd3ba092630c616aa5001984582db26449b
Ezz2 <94149479+Ezz2@users.noreply.github.com>
2022-06-28 14:58:22 +0200

    Kivy Module

---
+++ Kivy/kivy.py
@@ -0,0 +1,14 @@
+# By Ezz
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class Test(App):
+    
+    def build(self):
+        
+        return Label (text="Hello Iam Ezz")
+    
+if __name__ == "__main__":
+    
+    Test().run()
