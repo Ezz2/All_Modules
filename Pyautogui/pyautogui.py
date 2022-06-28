b0b9d46aea1c0bf4cfaac29fe50d7bfa94df38cf
Ezz2 <94149479+Ezz2@users.noreply.github.com>
2022-06-28 15:01:32 +0200

    Pyautogui Module

---
+++ Pyautogui/pyautogui.py
@@ -0,0 +1,12 @@
+# By Ezz
+
+import pyautogui
+
+pyautogui.hotkey("win", "3")
+pyautogui.hotkey("ctrl", "t")
+pyautogui.moveTo(435, 64)
+pyautogui.typewrite("instagram.com/3zooz.73/")
+pyautogui.hotkey("enter")
+
+# print(pyautogui.KEYBOARD_KEYS)
+# print(pyautogui.displayMousePosition())
